0bd31a96621be0f49ae376663f67959966602b03
feriel-web <ferielkhalouf45@gmail.com>
2026-09-06 17:02:30 +0200

    change

--- main.c
+++ main.c
@@ -113,6 +113,6 @@ int main() {
     evaluer_robustesse(mot_de_passe);
     sauvegarder_historique(mot_de_passe);
     printf("==========================================\n");
-printf("fin de projet");
+
     return 0;
 }
